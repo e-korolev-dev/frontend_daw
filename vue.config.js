325a2864d74584c52131ee0a5efe6e42fc0e1f67
e.korolev <e.korolev@techmoney.ru>
2024-07-30 09:50:28 +0700

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  devServer: {
+    port: 8888, // Изменение порта на 8888
+  },
   transpileDependencies: true,
 });
